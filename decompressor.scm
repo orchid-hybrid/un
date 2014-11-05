ec1b20b88333110ea5dc16321cd5b8a436625677
yuuko <yuuko@users.noreply.github.com>
2014-11-05 17:38:07 +0000

    added support for a decompressor to have multiple extensions

--- decompressor.scm
+++ decompressor.scm
@@ -7,10 +7,12 @@
 
 (define-syntax define-decompressor
   (syntax-rules ()
+    ((define-decompressor (<extension> ...) <tool> <invocation> <error-exit-codes>)
+     (begin
+       (define-decompressor <extension> <tool> <invocation> <error-exit-codes>) ...))
     ((define-decompressor <extension> <tool> <invocation> <error-exit-codes>)
      (push! decompressors (list <extension> <tool> <invocation> '<error-exit-codes>)))))
 
 (define decompressor-tool second)
 (define (decompressor-invocation de name dir) ((third de) name dir))
 (define (decompressor-error-code? de code) (member code (fourth de)))
-
--- decompressor/tar.scm
+++ decompressor/tar.scm
@@ -1,4 +1,4 @@
-(define-decompressor "tar"
+(define-decompressor ("tgz" "tar.gz" "tbz" "tbz2" "tb2" "tar.bz2" "taz" "tz" "tar.z" "tlz" "tar.lz" "tar.lzma" "txz" "tar.xz")
   "tar"
   (lambda (filename directory)
     (list "-xf" filename (string-append "-C" directory)))
